501c16e423dabb5eea374ce83c338cc016fb5b88
patrickaregan <patrickaregan@gmail.com>
2022-01-28 23:55:08 -0600

    script creation of time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,76 @@
 
 var currentDate = moment().format("dddd, MMMM Do");
-
+var currentHour = moment().format("H");
+console.log("Current Hour: " + currentHour);
 
 $("#currentDay").text(currentDate);
+
+var buildTimeBlocks = function(hour) {
+
+    console.log("inside buildTimeBlocks and hour is: " + hour);
+
+    // create the row
+    var rowEl = $("<div>");
+    rowEl.addClass("row no-gutters");
+
+    // create column 1
+    var col1El = $("<div>");
+    col1El.addClass("col-sm-12 col-md-1 col-lg-1");
+    // create column 1 content
+    var col1ContentEl = $("<div>");
+    col1ContentEl.addClass("hour-block");
+    switch (hour) {
+        case 9 :
+        case 10:
+        case 11:
+            col1ContentEl.html("<span>" + hour + "AM</span>");
+            break;
+        case 12:
+            col1ContentEl.html("<span>" + hour + "PM</span>");
+            break;
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+            var hour12 = hour - 12;
+            col1ContentEl.html("<span>" + hour12 + "PM</span>");
+            break;
+    }
+    // add column 1 content to column 1
+    col1El.append(col1ContentEl);
+    // add column 1 to row
+    rowEl.append(col1El);
+
+    // create column 2
+    var col2El = $("<div>");
+    col2El.addClass("col-sm-12 col-md-10 col-lg-10");
+    // create column 2 content
+    var col2ContentEl = $("<div>");
+    col2ContentEl.addClass("time-block");
+    if (hour < currentHour) {
+        col2ContentEl.html(`<textarea id='hour${hour}' class='past'></textarea>`);
+    }
+    if (hour == currentHour) {
+        col2ContentEl.html(`<textarea id='hour${hour}' class='present'></textarea>`);
+    }
+    if (hour > currentHour) {
+        col2ContentEl.html(`<textarea id='hour${hour}' class='future'></textarea>`);
+    }
+    // add column 2 content to column 2
+    col2El.append(col2ContentEl);
+    // add column 2 to row
+    rowEl.append(col2El);
+
+    // create column 3
+    var col3El = $("<div>");
+    col3El.addClass("col-sm-12 col-md-1 col-lg-1");
+    rowEl.append(col3El);
+
+
+    $(".container").append(rowEl);
+}
+
+for (var i = 9; i < 18; i++) {
+    buildTimeBlocks(i);        
+}
